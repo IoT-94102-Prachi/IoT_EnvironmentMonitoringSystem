698e52a0a2554785331039a8443e263a5e89da82
PrachiKore <23uet053@dkte.ac.in>
2026-01-01 12:30:21 +0530

    final_project

---
+++ final_IoT_project/mqtt_sub_mysql.py
@@ -0,0 +1,53 @@
+import json
+import paho.mqtt.client as mqtt
+import mysql.connector
+import requests
+
+# ThingSpeak
+THINGSPEAK_API = "UH1UGT579EIDFNDR"
+
+# MySQL Connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="environment_monitor"
+)
+cursor = db.cursor()
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode())
+
+    temperature = data['temperature']
+    humidity = data['humidity']
+    gas = data['gas']
+
+    # Insert into MySQL
+    sql = """INSERT INTO parameter
+             (temperature, humidity, gas_level)
+             VALUES (%s, %s, %s)"""
+    cursor.execute(sql, (temperature, humidity, gas))
+    db.commit()
+
+    print("Inserted:", temperature, humidity, gas)
+
+    # Send to ThingSpeak
+    url = (
+        f"https://api.thingspeak.com/update?"
+        f"api_key={THINGSPEAK_API}"
+        f"&field1={temperature}"
+        f"&field2={humidity}"
+        f"&field3={gas}"
+    )
+    requests.get(url)
+
+# MQTT Client (NEW API – NO WARNING)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+client.connect("test.mosquitto.org", 1883, 60)
+client.subscribe("iot/environment")
+client.on_message = on_message
+
+print("Subscriber started...")
+client.loop_forever()
+
